c7d7bc63711367b604bf492dbd5f231a2caf854b
ovenbakedgoods <ovenbakedgoods88@gmail.com>
2021-08-04 03:26:11 -0500

    Updating routes for courses and teachers

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from "./Home";
-
+import About from "./About";
+import Courses from './Courses'; 
+import Teachers from './Teachers';
+import Header from './Header';
 const App = () => (
   <BrowserRouter>
   <div className="container">
+  <Header />
   <Route exact path = "/" component = {Home} />
+  <Route path = "/about" component = {About} />
+  <Route path = "/teachers" component = {Teachers} />
+  <Route path = "/courses" component = {Courses} />
   </div>
   </BrowserRouter>
 );
--- src/components/Courses.js
+++ src/components/Courses.js
@@ -1,18 +1,22 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 
 const Courses = () => (
+<BrowserRouter>
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <li><a href='/courses/html'>HTML</a></li>
-        <li><a href='/courses/css'>CSS</a></li>
-        <li><a href='/courses/javascript'>JavaScript</a></li>
+        <li><NavLink to ='/courses/html'>HTML</NavLink></li>
+        <li><NavLink to ='/courses/css'>CSS</NavLink></li>
+        <li><NavLink to ='/courses/javascript'>JavaScript</NavLink></li>
       </ul>
     </div>
     
-    {/* Write routes here... */}
+    {<Route path = "/courses/html" component = {Courses}/>}
   </div>
+  </BrowserRouter>
 );
 
 export default Courses;
